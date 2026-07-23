e46b498aa66724d70191ee6a03542e4c4bf1c96c
Nihar Ranjan <niharranjanchetia022@gmail.com>
2026-07-23 15:17:21 +0530

    Switch to Google Gemini 1.5 Flash

--- utils/llm.py
+++ utils/llm.py
@@ -1,14 +1,12 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-llm= HuggingFaceEndpoint(
-    repo_id="meta-llama/Llama-3.1-8B-Instruct",
-    # repo_id='Qwen/Qwen2.5-Coder-7B-Instruct',
-    task="chat-completion",
-    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+    temperature=0
 )
-model=ChatHuggingFace(llm=llm)
 def get_llm():
     return model
